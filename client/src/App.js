1f1e56e847df4b48425af7a86124ad6e93b37123
hasherlaws18 <houstonasherlaws@gmail.com>
2023-05-11 21:18:16 -0500

    Tried to fix some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,38 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import {
-  ApolloServer,
+  ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  creatHttpLink,
+  createHttpLink,
 } from '@apollo/client';
+import {setContext} from "@apollo/client/link/context"
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -24,6 +46,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
